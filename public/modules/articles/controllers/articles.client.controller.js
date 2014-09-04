ef0102050d4cb35f7679c4112985bf983b0c878a
Filipe Mendes Arruda <filipemarruda@gmail.com>
2014-09-04 02:57:36 -0300

    adicao do editor nos artigos

--- public/modules/articles/controllers/articles.client.controller.js
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -4,6 +4,27 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
 	function($scope, $stateParams, $location, Authentication, Articles) {
 		$scope.authentication = Authentication;
 
+		$scope.tinymceOptions = {
+		    theme: "modern",
+		    plugins: [
+		        "advlist autolink lists link charmap print preview anchor",
+		        "searchreplace visualblocks code fullscreen",
+		        "insertdatetime media table contextmenu paste emoticons code"
+		    ],
+		    menubar: false,
+        	toolbar_items_size: 'small',
+        	style_formats: [
+	                {title: 'Bold text', inline: 'b'},
+	                {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+	                {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+	                {title: 'Example 1', inline: 'span', classes: 'example1'},
+	                {title: 'Example 2', inline: 'span', classes: 'example2'},
+	                {title: 'Table styles'},
+	                {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+	        ],
+		    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link emoticons | preview code"
+		};
+
 		$scope.create = function() {
 			var article = new Articles({
 				title: this.title,
